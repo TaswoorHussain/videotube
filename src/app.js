9a8a0e73d17a275ed138906408c0bfc345bc2bd9
TaswoorHussain <devtaswoor>
2023-12-02 13:33:07 +0500

    add middlewares in app.js file

--- src/app.js
+++ src/app.js
@@ -11,4 +11,20 @@ app.use(cors({
 }))
 
 
+app.use(express.json({
+    limit:"16kb"
+}))
+
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb",
+}))
+
+
+app.use(express.static("public"));
+
+
+
+app.use(cookieParser());
 export {app}
